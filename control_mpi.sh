61ad57aaa4634a5977b017bf57c3b3384e8ae933
Joshua Robinson <joshua@purestorage.com>
2020-12-05 14:30:25 -0800

    Update control script for io500

--- control_mpi.sh
+++ control_mpi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Image and repository name to pull images.
-IMGNAME=joshuarobinson/ior
+IMGNAME=joshuarobinson/io500
 
 # NFS Data VIP and filesystem name to test against. Assumes filesystem already
 # created and configured with NFS access.
@@ -12,7 +12,7 @@ DATAHUB_FS="ior-benchmark"
 HOSTGROUP="irp210"
 
 # Degree of concurrency to use in testing.
-CONC=960
+CONC=600
 
 # Port range used by MPI, configured in Dockerfile.
 PORTRANGE="24000-24100"
@@ -24,31 +24,44 @@ if [ "$1" == "start" ]; then
 	./build_image.sh
 	ansible $HOSTGROUP -a "docker pull $IMGNAME"
 
-	# Create a hostfile group from ansible to inject into master when
-	# running tests so that it can find all  workers.
-	if [ ! -e hostfile ]; then
-		echo "Generating 'hostfile' from ansible group $HOSTGROUP"
-		ansible-inventory --list $HOSTGROUP | jq ".$HOSTGROUP.hosts[]" | xargs dig +short +search > hostfile
-	fi
+    # Create a hostfile group from ansible to inject into master when
+    # running tests so that it can find all  workers.
+    if [ ! -e hostfile ]; then
+        echo "Generating 'hostfile' from ansible group $HOSTGROUP"
+        ansible-inventory --list $HOSTGROUP | jq ".$HOSTGROUP.hosts[]" | xargs dig +short +search > hostfile
+    fi
+
+    echo "Recreating test filesystem."
+    ansible $HOSTGROUP:localhost -b -a "umount /mnt/io500"
+    ./recreate-test-filesystem.py
+
+    ansible $HOSTGROUP:localhost -b -a "mkdir -p /mnt/io500"
+    ansible $HOSTGROUP:localhost -b -a "mount $DATAHUB_IP:/$DATAHUB_FS /mnt/io500"
+
 
 	# Create docker volume for NFS mount to test.
-	ansible $HOSTGROUP -a "docker volume create --driver local --opt type=nfs --opt o=addr=$DATAHUB_IP,rw \
-		--opt=device=:/$DATAHUB_FS iorscratch"
+	#docker volume create --driver local --opt type=nfs --opt o=addr=$DATAHUB_IP,rw \
+	#	--opt=device=:/$DATAHUB_FS iorscratch
+	#ansible $HOSTGROUP -a "docker volume create --driver local --opt type=nfs --opt o=addr=$DATAHUB_IP,rw \
+    #	--opt=device=:/$DATAHUB_FS iorscratch"
 
-	# Start worker containers.
+	echo "Start worker containers."
 	ansible $HOSTGROUP -a "docker run -d --rm \
 		-p 2222:22 \
 		-p $PORTRANGE:$PORTRANGE \
 		--name=mpi-ior \
 		--add-host=ior-master:$(hostname -i) \
-		-v iorscratch:/scratch \
+		-v /mnt/io500:/datafiles \
 		$IMGNAME \
 		sudo /usr/sbin/sshd -D"
 
 elif [ "$1" == "stop" ]; then
 	echo "Stopping MPI workers"
+    docker stop ior-master
 	ansible $HOSTGROUP -a "docker stop mpi-ior"
-	ansible $HOSTGROUP -a "docker volume rm iorscratch"
+	#ansible $HOSTGROUP -a "docker volume rm iorscratch"
+    ansible $HOSTGROUP:localhost -b -a "umount /mnt/io500"
+	#docker volume rm iorscratch
 
 elif [ "$1" == "run" ]; then
 
@@ -58,14 +71,20 @@ elif [ "$1" == "run" ]; then
 		echo "Running IOR benchmark"
 		# -B: use ODIRECT IO
 		# -F: use a unique file per worker
-		TESTCMD="ior -v -B -w -r -i 3 -F -o /scratch/foo/iorfile -t 4M -b 2G"
+		#TESTCMD="ior -v -B -w -r -i 3 -F -o /datafiles/foo/iorfile -t 4M -b 2G"
+        TESTCMD="ior -w -r -d /datafiles/foo "
 	
 	elif [ "$2" == "mdtest" ]; then
 		echo "Running mdtest benchmark"
 		# -b: branching factor
 		# -z: tree depth
 		# -I: items per directory
-		TESTCMD="mdtest -b 3 -v -z 3 -u -I 3000 -i 1 -d /scratch/foo"
+		#TESTCMD="mdtest -b 3 -v -z 3 -u -I 5000 -i 1 -d /datafiles/foo"
+		TESTCMD="mdtest -v -u -n 5000 -i 3 -d /datafiles/foo"
+	
+    elif [ "$2" == "io500" ]; then 
+        echo "Running io500 benchmark"
+        TESTCMD="io500/io500 io500/config-all.ini -v=0"
 	
 	else
 		echo "Error $2 not a valid benchmark. Options: ior|mdtest"
@@ -73,13 +92,20 @@ elif [ "$1" == "run" ]; then
 	fi
 
 	# Launch test using the localhost to run the master.	
-	docker run -it --rm --name=ior-master \
+	docker run -it --name=ior-master \
 		--hostname=ior-master \
 		-p $PORTRANGE:$PORTRANGE \
-		-v iorscratch:/scratch \
+		-v iorscratch:/datafiles \
 		-v ${PWD}/hostfile:/project/hostfile \
 		$IMGNAME \
 		mpiexec -f /project/hostfile -n $CONC $TESTCMD
+
+    #docker cp ior-master:/project/io500/results results/
+    #docker rm ior-master
+
+elif [ "$1" == "restart" ]; then
+    # Stop and then start.
+    $0 stop; $0 start
 
 else
 	echo "Usage: $0 [start|stop|run]"
